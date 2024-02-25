4c0f943f8a2c885d56f69ef2fcef690123592011
Mr.Resor <jakusienko@gmail.com>
2024-02-25 15:01:33 +0100

    some initial code.

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config(); //initialize dotenv
+const Discord = require('discord.js'); //import discord.js
+var cron = require("cron");
+
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMembers
+    ]
+}); //create new client
+
+//add job to check once a day any reports that are week old
+//var job = new cron.CronJob('00 37 6,13,19 * * *', sendReminder); //main job
+
+// login init
+client.on('ready', () => {
+    //add db connection
+    console.log(`Logged in as ${client.user.tag}!`);
+    //job.start();
+});
+
+//main job function call
+function check_old_reports() {
+    // chceck what plannets were scanned more than a week ago
+}
+
+
+
+//check messages
+client.on('messageCreate', async msg => {
+    // just in case to stop bots, may have to change behaviour later
+    if (msg.author.bot) return;
+    
+        if (regx.test(msg.content) === true) {
+            msg.reply(get_desired_content(msg.content)).catch(console.error);
+        } else if (msg.content === 'ping') {
+            msg.reply("pong <:jebacpolsl:987745197087686718>")
+                .catch(console.error);
+        }
+});
+
+//make sure this line is the last line
+client.login(process.env.CLIENT_TOKEN); //login bot using token
