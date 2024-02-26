0598a39942d2ca3aff805794f78d3cb6155ef1f8
Mr.Resor <jakusienko@gmail.com>
2024-02-26 14:14:51 +0100

    Created function for returning messages.

---
+++ messages.js
@@ -0,0 +1,15 @@
+const msg = {
+    unknown_command: 'Unknown command. Type !help for information about available commands.',
+    wrong_channel1: 'Incorrect channel, please use <#',
+    wrong_channel2: '> for any bot commands.',
+    help_short: 'Available commands:\n'
+        + '!add_user\t->\tAdds user to the data base, which allows for certain calculations when using other functions of this bot.\n',
+    help_full: '!add_user\t->\tRequires a string from second API available in the fleet menu, but cut out after research.\n'
+        + 'Example: "!add_user {"coords":"0:000:0","characterClassId":0,"allianceClassId":0,"research":{"109":8, "110":7, "111": 9, "114": 4, "115": 11, "117": 6, "118": 3"}}  '
+}
+
+module.exports = {
+    text: function (msg_str) {
+        return msg[msg_str]
+    }
+};
