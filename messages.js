9dd8d99d952b1a998e917111c263f57e2ab91aad
Mr.Resor <jakusienko@gmail.com>
2024-02-28 13:25:49 +0100

    Changed data in example.

--- messages.js
+++ messages.js
@@ -5,7 +5,7 @@ const messages = {
     help_short: 'Available commands:\n'
         + '!add_user\t->\tAdds user to the data base, which allows for certain calculations when using other functions of this bot.\n',
     help_full: '!add_user\t->\tRequires a string from second API available in the fleet menu, but cut out after research.\n'
-        + 'Example: "!add_user {"coords":"0:000:0","characterClassId":0,"allianceClassId":0,"research":{"109":8, "110":7, "111": 9, "114": 4, "115": 11, "117": 6, "118": 3"}}',
+        + 'Example: "!add_user {"coords":"0:000:0","characterClassId":0,"allianceClassId":0,"research":{"109":0, "110":0, "111": 0, "114": 0, "115": 0, "117": 0, "118": 0"}}',
     wrong_data: 'Incorrect input, please type "!help" to see information about correct one.'
 }
 
