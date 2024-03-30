284e959c45dbc0688560e8a2537a320becf46837
Mr.Resor <jakusienko@gmail.com>
2024-03-30 21:07:41 +0100

    Removed old files.

--- util.js
+++
@@ -1,9 +0,0 @@
-// function for comparing arrays i found on the internet
-function array_equals(a, b) {
-    return Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index]);
-}
-
-export { array_equals };
